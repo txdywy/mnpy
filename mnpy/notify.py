266829a589ceaf016b80d0b9eeb012027afbee40
sandbox <appflood_sandbox@papayamobile.com>
2015-06-10 02:12:32 +0000

    fix order

--- mnpy/notify.py
+++ mnpy/notify.py
@@ -1,14 +1,14 @@
 from model import *
 import util
 import datetime
-from sqlalchemy import and_
+from sqlalchemy import and_, desc
 
 #MNPY_RECEIVER = 'weiyi@papayamobile.com'
 MNPY_RECEIVER = 'appflood_engineer@papayamobile.com'
 
 MNPY_SNIPPET = """
 <h4 style="margin:0;margin-bottom:6px;margin-top:6px">
-<a style="font-size:14px;line-height:22px;font-weight:bold;text-decoration:none;color:#259;border:none;outline:none" href="%s" target="_blank">%s</a>&nbsp;&nbsp;</h4>
+<a style="font-size:14px;line-height:22px;font-weight:bold;text-decoration:none;color:#259;border:none;outline:none" href="%s" target="_blank">%s[week %s]</a>&nbsp;&nbsp;</h4>
 """
 
 def notify(body='\xe4\xbb\x80\xe4\xb9\x88\xe9\x83\xbd\xe6\xb2\xa1\xe6\x9c\x89......'):
@@ -19,21 +19,21 @@ def notify(body='\xe4\xbb\x80\xe4\xb9\x88\xe9\x83\xbd\xe6\xb2\xa1\xe6\x9c\x89...
 def task():
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(1)
-    mps = ManongPython.query.filter(and_(ManongPython.create_time>yesterday, ManongPython.create_time<today)).all()
+    mps = ManongPython.query.filter(and_(ManongPython.create_time>yesterday, ManongPython.create_time<today)).order_by(desc(ManongPython.page)).all()
     #mps = ManongPython.query.all()
     print '=============', len(mps)
-    body = MNPY_SNIPPET % ('http://appflood.ml/mnpy.html', u'\u6240\u6709\u7ed3\u679c\u7684\u9875\u9762')
+    body = MNPY_SNIPPET % ('http://appflood.ml/mnpy.html', u'\u6240\u6709\u7ed3\u679c\u7684\u9875\u9762', 'all')
     if mps:
         for mp in mps:
-            body += MNPY_SNIPPET % (mp.url, mp.title)
+            body += MNPY_SNIPPET % (mp.url, mp.title, mp.page)
         notify(body)
         html()
 
 def html():
-    mps = ManongPython.query.all()
+    mps = ManongPython.query.order_by(desc(ManongPython.page)).all()
     body = '<meta charset="UTF-8">'
     for mp in mps:
-        body += MNPY_SNIPPET % (mp.url, mp.title)
+        body += MNPY_SNIPPET % (mp.url, mp.title, mp.page)
     #print body
     with open('/var/server/www/mnpy.html', 'w') as f:
         f.write(body.encode('utf8'))
